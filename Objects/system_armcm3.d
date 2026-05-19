0e2055822a65164fcff81839b8e46adb7b47f416
xhtx0510 <1502351695@qq.com>
2026-05-19 17:20:38 +0800

    Init

---
+++ Objects/system_armcm3.d
@@ -0,0 +1,4 @@
+./objects/system_armcm3.o: RTE\Device\ARMCM3\system_ARMCM3.c \
+  D:\RM\Microcontroller\STM32\keil5MDK\ARM\Cortex_DFP\1.1.0\Device\ARMCM3\Include\ARMCM3.h \
+  D:\RM\Microcontroller\STM32\keil5MDK\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm3.h \
+  D:\RM\Microcontroller\STM32\keil5MDK\ARM\Cortex_DFP\1.1.0\Device\ARMCM3\Include\system_ARMCM3.h
---
+++ main.c
@@ -0,0 +1,19 @@
+
+int fun0();
+int fun1();
+
+int fun0()
+{
+	return 1;
+}
+
+
+int main()
+{
+	while(1)
+	{
+		fun0();
+	}
+	return 0;
+
+}
